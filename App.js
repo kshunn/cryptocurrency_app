8115c67edfee0f4a1a0cacf0d2b3db868dc58d76
kshunn <qkshunn@gmail.com>
2020-03-03 23:36:00 +0900

    axios installed / Loading.js, Coin.js added

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
+import Coin from './Coin';
+import axios from 'axios';
+
+const API_KEY;
+
+export default class extends React.Component {
+  state = {
+    isLoading = true
+  };
+
+  render(){
+    return null;
+  }
+}
